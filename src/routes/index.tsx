d5e7491170212d196cd9c82fdf611d920f2da005
Luis Almonte <lalmonte@collette.com>
2024-12-15 14:39:54 -0500

    additional lint cleanup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "@/components/layout";
 import Home from "@/pages";
 import Movies from "@/pages/movies";
 import { MovieDetail } from "@/pages/movie/id";
 import TrendingPage from "@/pages/trending";
+import UpcomingPage from "@/pages/upcoming";
 import NotFound from "@/pages/not-found";
 
 export default function AppRoutes() {
@@ -14,6 +14,7 @@ export default function AppRoutes() {
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
         <Route path="trending" element={<TrendingPage />} />
+        <Route path="upcoming" element={<UpcomingPage />} />
         <Route path="movie/:id" element={<MovieDetail />} />
         <Route path="*" element={<NotFound />} />
       </Route>
